771874a51f5d32df02dceba9369e56484e79b88e
Matt Goeckel <mattgoeckel13@gmail.com>
2023-07-30 22:34:45 -0500

    Name edits, tasks 8, 9A, & 10

--- continuous_stock.py
+++ continuous_stock.py
@@ -27,6 +27,7 @@ We usually include a .env-example file to illustrate the format.
 import asyncio
 import os
 from random import randint
+import datetime
 
 #External Imports
 import pandas as pd
@@ -65,4 +66,50 @@ def init_csv_file(file_path):
     df_empty.to_csv(file_path, index=False) 
 
 async def update_csv_stock():
-    
+    """Update the CSV file with the latest stock information."""
+    logger.info("Calling update_csv_stock")
+    try:
+        companies = ["Wolverine World Wide Inc", "Nice Inc", "Lululemon Athletica Inc", "Under Armour Inc", "On Holding AG"]
+        update_interval = 60  # Update every 1 minute (60 seconds)
+        total_runtime = 15 * 60  # Total runtime maximum of 15 minutes
+        num_updates = 10  # Keep the most recent 10 readings
+        logger.info(f"update_interval: {update_interval}")
+        logger.info(f"total_runtime: {total_runtime}")
+        logger.info(f"num_updates: {num_updates}")
+
+        # Use a deque to store just the last, most recent 10 readings in order
+        records_deque = deque(maxlen=num_updates)
+
+        fp = Path(__file__).parent.joinpath("data").joinpath("stock.csv")
+
+        # Check if the file exists, if not, create it with only the column headings
+        if not os.path.exists(fp):
+            init_csv_file(fp)
+
+        logger.info(f"Initialized csv file at {fp}")
+
+        for _ in range(num_updates):  # To get num_updates readings
+            for company in companies:
+                ticker = lookup_ticker(company)
+                new_price = await get_stock_price(ticker)
+                time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
+                new_record = {
+                    "Company": company,
+                    "Ticker": ticker,
+                    "Time": time_now,
+                    "Price": new_price,
+                }
+                records_deque.append(new_record)
+
+            # Use the deque to make a DataFrame
+            df = pd.DataFrame(records_deque)
+
+            # Save the DataFrame to the CSV file, deleting its contents before writing
+            df.to_csv(fp, index=False, mode="w")
+            logger.info(f"Saving stocks to {fp}")
+
+            # Wait for update_interval seconds before the next reading
+            await asyncio.sleep(update_interval)
+
+    except Exception as e:
+        logger.error(f"ERROR in update_csv_stock: {e}")
